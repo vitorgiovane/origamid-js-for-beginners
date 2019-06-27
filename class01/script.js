2147ddc6b429a703a8a1239d1e19e638f67e1ea9
Vitor <vitorgpalves@gmail.com>
2019-06-27 00:56:58 -0300

    feature: finishes the class 01

---
+++ class01/script.js
@@ -0,0 +1,19 @@
+var firstName = "Vitor"
+let middleName = "Giovane Pereira"
+const lastName = "Alves"
+
+const age = "25"
+
+var favoriteFood
+favoriteFood = "Beef"
+
+var car = "Onix"
+let manufacturer = "GM"
+const color = "Black"
+let year = 2019
+const type = "automatic"
+
+console.log(firstName, middleName, lastName)
+console.log(age)
+console.log(favoriteFood)
+console.log(car, manufacturer, color, year, type)
