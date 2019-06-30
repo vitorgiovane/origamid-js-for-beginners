43d686bcd70bcf89d7713c4f9f5be3a3b891b3b7
Vitor <vitorgpalves@gmail.com>
2019-06-30 03:38:24 -0300

    feature: finishes the class 02

---
+++ class02/script.js
@@ -0,0 +1,8 @@
+const name = "Vitor"
+const numberIntoString = "25"
+const age = 25
+const myName = "Vitor"
+const myLastName = "Alves"
+const fullName = `${myName} ${myLastName}`
+const statement = "It's time"
+console.log(typeof fullName)
