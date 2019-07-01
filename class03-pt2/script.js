119ab2ad5ceafdea140b0a1a4344959e3ade6f45
Vitor <vitorgpalves@gmail.com>
2019-06-30 23:21:57 -0300

    feature: finishes the second part of the class 03

---
+++ class03-pt2/script.js
@@ -0,0 +1,24 @@
+const age = "25"
+console.log(age)
+const title = +age + 25
+console.log(title)
+
+// NaN
+console.log(+"Lorem Ipsum")
+console.log("Lorem" * 2)
+
+// Sum string with number
+const firstOperator = "200"
+const secondOperator = 50
+const totalOfOperation = +firstOperator + secondOperator
+console.log(totalOfOperation)
+
+// Incrementation
+let incrementationValue = 5
+console.log(++incrementationValue)
+
+// Division
+const number = +"80" / 2
+const unity = "kg"
+const weight = number + unity
+console.log(weight)
