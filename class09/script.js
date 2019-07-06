3a6f8eb81f948896e027d564c5188f0bf7ec3874
Vitor <vitorgpalves@gmail.com>
2019-07-06 20:39:39 -0300

    feature: finishes the class 09

---
+++ class09/script.js
@@ -0,0 +1,13 @@
+// Exercises
+
+// 01
+let scroll = 1000
+scroll += 500
+console.log(scroll)
+
+// 02
+const hasCar = false
+const hasHome = true
+
+const giveCredit = hasCar && hasHome
+console.log("giveCredit", giveCredit)
